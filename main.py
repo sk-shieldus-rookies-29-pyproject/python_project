d9d5cde29e1620b0e52323da32551e1d20b8acff
yeonjae02 <0yeonjae2@naver.com>
2025-11-17 15:53:02 +0900

    feat: 프로젝트 메인 함수에 rss 기능 호출  추가

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import google_calendar_use
 import slack_weather
+import python_rss
 
 def main():
     print("=== 통합 작업 시작 ===")
@@ -17,6 +18,13 @@ def main():
         print("   -> 날씨 작업 완료")
     except Exception as e:
         print(f"   [오류] 날씨 작업 실패: {e}")
+
+    print("\n>> [Step 3] 보안 뉴스 rss 작업 실행")
+    try:
+        python_rss.rss_boannews()
+        print("   -> 뉴스 작업 완료")
+    except Exception as e:
+        print(f"   [오류] 뉴스 작업 실패: {e}")
 
     print("\n=== 모든 작업이 종료되었습니다 ===")
 
