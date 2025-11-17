b9bc4c063215f4aa1d48181b81726d2a55598f20
yeonjae02 <0yeonjae2@naver.com>
2025-11-17 15:52:42 +0900

    feat: 메인함수에서 호출 가능한 형태로 함수 추가 및 slack 전송 기능 추가

--- python_rss.py
+++ python_rss.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request
 import feedparser
+import requests
+import config
+
+def send_to_slack(text):
+    webhook_url = config.SLACK_WEBHOOK_URL
+    payload = { 'text': text }
+    requests.post(webhook_url, json=payload)
 
 #보안뉴스 rss 이용해 기사 3개 가져와 출력하기
 def rss_boannews():
@@ -11,12 +18,18 @@ def rss_boannews():
 
     # number는 기사 개수
     number=1
+
+    slack_message = "📢 보안뉴스 주요 내용\n\n"
     for entry in feed.entries:
         # 기사 제목과 링크, 날짜 출력
         print(f'{number}\n{entry.title}\n{entry.link}\n{entry.updated}\n')
+        slack_message += f"{number}. {entry.title}\n🔗 {entry.link}\n🕒 {entry.updated}\n\n"
         number+=1
         # 기사는 3개까지 
         if number==4:
             break
 
-rss_boannews()
+    send_to_slack(slack_message)
+
+if __name__ == '__main__':
+    rss_boannews()
